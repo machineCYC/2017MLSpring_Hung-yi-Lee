148bd2dba144a8ee39d909bfe67bb6178163dbb0
CYC <yenchen0416@gmail.com>
2018-01-14 14:12:08 +0800

    add loss and accuracy plot

--- HW3/main.py
+++ HW3/main.py
@@ -22,13 +22,14 @@ def buildModel(mode):
     
     if mode == "cnn":
         model.add(Convolution2D(32, kernel_size=3, strides=1, padding="same", input_shape=(48, 48, 1)))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-
+        # model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
         model.add(Convolution2D(64, kernel_size=3, strides=1, padding="same"))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-
+        # model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
         model.add(Convolution2D(128, kernel_size=3, strides=1, padding="same"))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
+        # model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 
         model.add(Flatten())
         model.add(Dense(128))
@@ -55,10 +56,33 @@ if __name__ == "__main__":
     arrayImage = DataTrain["Image.npy"]/255.
 
     model = buildModel(mode="cnn")
-    model.fit(arrayImage, arrayLabel, epochs=15, batch_size=128, verbose=2, validation_split=0.25, shuffle=True)
+    # keras.utils.plot_model(model, to_file=os.path.join(strProjectFolder, "02-Output/model.png"), show_shapes=True)
+    Training = model.fit(arrayImage, arrayLabel, epochs=15, batch_size=128, verbose=2, validation_split=0.25, shuffle=True)
+
+    # model.save(os.path.join(strProjectFolder, "02-Output/model"))
+
+    # Loss Curves
+    plt.figure(figsize=[8, 6])
+    plt.plot(Training.history["loss"], "r", linewidth=3.0)
+    plt.plot(Training.history["val_loss"], "b", linewidth=3.0)
+    plt.legend(["Training loss", "Validation Loss"], fontsize=18)
+    plt.xlabel("Epochs ", fontsize=16)
+    plt.ylabel("Loss", fontsize=16)
+    plt.title("Loss Curves", fontsize=16)
+    
+    # Accuracy Curves
+    plt.figure(figsize=[8, 6])
+    plt.plot(Training.history["acc"], "r", linewidth=3.0)
+    plt.plot(Training.history["val_acc"], "b", linewidth=3.0)
+    plt.legend(["Training Accuracy", "Validation Accuracy"], fontsize=18)
+    plt.xlabel("Epochs ", fontsize=16)
+    plt.ylabel("Accuracy", fontsize=16)
+    plt.title("Accuracy Curves", fontsize=16)
+    plt.show()
 
 
 
     # listLabel, listImageVector, listImage = makeDataProcessing(DataTest)
-    # arrayLabel = keras.utils.to_categorical(listLabel)
     # classes = model.predict(listImageVector, batch_size=128)
+
+
