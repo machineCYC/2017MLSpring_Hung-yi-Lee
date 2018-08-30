4ad7ec5b5813564751625af6a95796eac95c3d14
CYC <yenchen0416@gmail.com>
2018-08-30 23:03:43 +0800

    finish part1 pca

--- HW6/pca.py
+++ HW6/pca.py
@@ -23,7 +23,7 @@ class prepareImage():
         In this process, it will do normalize for each image then resize the image.
         """
         for image in strFaceData:
-            arrayImage = io.imread(image, as_grey=False)
+            arrayImage = io.imread(image)
             arrayResizeImage = transform.resize(arrayImage, (self.int_Image_Size, self.int_Image_Size, 3))
             self.list_Images_Vector.append(arrayResizeImage.flatten())
 
@@ -33,18 +33,14 @@ class PCA(object):
         self.n_components = n_components
     
     def fit(self, X):
-        X = self._check_array(X)
+        X = self._check_array(X) # (N, D)
 
-        self.mean_ = np.mean(X, axis=0)
-        X -= self.mean_
+        self.mean_ = self._check_array(np.mean(X, axis=0)) # (1, D)
 
-        svd_star = time.time()
-        U, S, V = np.linalg.svd(X.T, full_matrices=False)
-        svd_end =  time.time()
-        print("use {} s".format(svd_end - svd_star))
+        U, S, V = np.linalg.svd((X - self.mean_).T, full_matrices=False)
 
         self.S = S[0:self.n_components]
-        self.U = U[:, 0:self.n_components]
+        self.U = U[:, 0:self.n_components] # (D, n_components)
         self.explained_variance_ratio_ = np.round(self.S / np.sum(self.S), 3)
         return
     
@@ -56,9 +52,9 @@ class PCA(object):
         return Z
     
     def inverse_transform(self, X):
-        Z = self.transform(X)
+        Z = self.transform(X) # (n, n_componet)
 
-        X_hat = np.dot(Z, self.U.T)
+        X_hat = np.dot(Z, self.U.T) #(n, D)
         return X_hat
 
     def _check_array(self, ndarray):
@@ -83,66 +79,23 @@ if __name__ == "__main__":
 
     pca = PCA(n_components=4)
     pca.fit(X=list_Images_Vector)
-    array_TopK_EigenVectors = pca.U # (D, 4)
+
+    list_Random_Index = [100, 200, 300, 400]
+    list_Random_Images_Vectors = [list_Images_Vector[i] for i in list_Random_Index]
+    array_Recon_Images_Vectors = pca.inverse_transform(X=list_Random_Images_Vectors)
+    ReconImage = array_Recon_Images_Vectors + pca.mean_
 
     # plot avg face
-    io.imsave(os.path.join(strOutputPath, "AvgFace.png"), pca.mean_.reshape(128, 128,3))
-    # plt.imshow(pca.mean_.reshape(128, 128, 3))
-    # plt.xticks(np.array([]))
-    # plt.yticks(np.array([]))
-    # plt.savefig(os.path.join(strOutputPath, "AvgFace"))
+    io.imsave(os.path.join(strOutputPath, "AvgFace.png"), pca.mean_.reshape(128, 128, 3))
 
     # plot top 4 eigen face
-    fig = plt.figure(figsize=(12, 2))
     for i in range(4):
-        ax = fig.add_subplot(1, 4, i+1)
-        # let each value in 0 ~ 255
-        array_Topi_EigenVectors = array_TopK_EigenVectors[:, i]
-
-        io.imsave(os.path.join(strOutputPath, "Top{}EigenFaces.png".format(i)), get_Img_Clip(array_Topi_EigenVectors))
-
-    #     ax.imshow(get_Img_Clip(array_Topi_EigenVectors))
-    #     plt.xticks(np.array([]))
-    #     plt.yticks(np.array([]))
-    #     plt.tight_layout()
-    # plt.savefig(os.path.join(strOutputPath, "Top{}EigenFaces".format(4)))
-
-    array_Random_Index = np.random.randint(0, 415, size=4)
-    list_Random_Images_Vectors = [list_Images_Vector[i] for i in array_Random_Index]
-    array_Recon_Images_Vectors = pca.inverse_transform(X=list_Random_Images_Vectors)
-
+        io.imsave(os.path.join(strOutputPath, "Top{}EigenFaces.png".format(i)), get_Img_Clip(pca.U[:, i]))
+    
     # plot 4 random reconstruct images
-    fig = plt.figure(figsize=(12, 4))
-    for i in range(4):
+    for i in range(len(list_Random_Index)):
+        io.imsave(os.path.join(strOutputPath, "Recon_img{}_{}Eigen.png".format(list_Random_Index[i], pca.n_components)), get_Img_Clip(ReconImage[i]))
+        io.imsave(os.path.join(strOutputPath, "Origin_img{}.png".format(list_Random_Index[i])), list_Images_Vector[list_Random_Index[i]].reshape(128, 128, 3))
 
-        ReconImage = array_Recon_Images_Vectors[i]
 
-        ReconImage += pca.mean_
-
-        # ReconImage -= np.min(ReconImage)
-        # ReconImage /= np.max(ReconImage)
-
-        # ReconImage = (ReconImage * 255).astype(np.uint8)
-        # ReconImage = np.reshape(ReconImage, (128, 128, 3))
-
-        io.imsave(os.path.join(strOutputPath, "ReconImage{}.png".format(array_Random_Index[i])), get_Img_Clip(ReconImage))
-        io.imsave(os.path.join(strOutputPath, "Origin{}.png".format(array_Random_Index[i])), list_Images_Vector[array_Random_Index[i]].reshape(128, 128,3))
-
-    #     ax = fig.add_subplot(2, 4, i+1)
-    #     ax.imshow(get_Img_Clip(ReconImage))
-    #     # ax.imshow(ReconImage)
-    #     plt.xticks(np.array([]))
-    #     plt.yticks(np.array([]))
-    #     plt.tight_layout()
-    #     plt.title("ReconImage{}".format(array_Random_Index[i]))
-
-    #     arrayOriginImage = list_Images_Vector[array_Random_Index[i]]
-
-    #     ax = fig.add_subplot(2, 4, i+5)
-    #     ax.imshow(arrayOriginImage.reshape((128, 128, 3)))
-    #     plt.xticks(np.array([]))
-    #     plt.yticks(np.array([]))
-    #     plt.tight_layout()
-    #     plt.title("Origin{}".format(array_Random_Index[i]))
-    # plt.savefig(os.path.join(strOutputPath, "Recon{}RandomImg{}Eigen".format(4, 4)))
 
